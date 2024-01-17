2b14cc5cce4b37b4f7ca2bc7d158bc552b38afd2
gsavichev <galesavich@gmail.com>
2024-01-17 05:58:34 +0500

    Доработанная элемент Item. Савичев

--- Elements/Item.xaml.cs
+++ Elements/Item.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,14 +16,21 @@ using System.Windows.Shapes;
 
 namespace savichev19pr.Elements
 {
-    /// <summary>
-    /// Логика взаимодействия для Item.xaml
-    /// </summary>
     public partial class Item : UserControl
     {
-        public Item()
+        public Item(Classes.Item item)
         {
             InitializeComponent();
+
+            if (item != null)
+            {
+                if (File.Exists(Directory.GetCurrentDirectory() + "/Images/" + item.src))
+                {
+                    image.Source = new BitmapImage(new Uri(Directory.GetCurrentDirectory() + "/Images/" + item.src));
+                }
+                price.Content = item.price;
+                name.Content = item.name;
+            }
         }
     }
 }
